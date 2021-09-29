89f5e8822a6e3f8e9bd459223442181af6ad71f5
Diveshan <diveshan1995@gmail.com>
2021-09-29 19:53:21 +0530

    completed all methods for post

--- server/controler/post.js
+++ server/controler/post.js
@@ -14,8 +14,25 @@ module.exports.createPost = async (req, res) => {
 }
 
 module.exports.deletePost = async (req, res) => {
-    const id = req.params._id
-    if(!mongoose.Types.ObjectId.isValid()) return res.status(400).send("No post found, to delete")
-    await Post.findByIdAndDelete(id)
-    res.send("Post deleted !")
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(400).send("No post found, to delete")
+    const post = await Post.findByIdAndDelete(id)
+    if(!post) return res.status(404).send("Post doesn't exist");
+    res.send("Post deleted ! : " + post)
+}
+
+module.exports.updatePost = async (req, res) => {
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(400).send("No post found, to delete")
+    const post = await Post.findByIdAndUpdate({_id: id}, req.body, { new: true, runValidators: true })
+    if(!post) return res.status(404).send("Post doesn't exist");
+    res.send("Post deleted ! : " + post)
+}
+
+module.exports.getSinglePost = async (req, res) => {
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(400).send("No post found")
+    const post = await Post.findById(id)
+    if(!post) return res.status(404).send('No post');
+    res.send(post)
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { getPosts, createPost, deletePost } = require('../controler/post')
+const { getPosts, createPost, deletePost, updatePost, getSinglePost } = require('../controler/post')
 const router = express.Router()
 
 router.get('/', getPosts)
 router.post('/', createPost)
+router.get('/:id', getSinglePost)
 router.delete('/:id', deletePost)
+router.patch('/:id', updatePost)
 
 module.exports = router
